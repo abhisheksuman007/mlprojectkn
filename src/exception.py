932fb5efb09fe36495f0765d9b2d655fb31fa67a
abhisheksuman007 <abhisheksuman.work@gmail.com>
2024-06-02 00:09:48 +0530

    Data Transformation done

--- src/exception.py
+++ src/exception.py
@@ -4,15 +4,15 @@ from src.logger import logging
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message="Error occured in Python Script name [{0}] line number [{1}] error message [{2}]".format()
-    file_name,exc_tb.tb_lineno,str(error)
-    
+    line_number = exc_tb.tb_lineno
+    #error_message=f"Error occured in Python Script name [{0}] line number [{1}] error message [{2}]".format()
+    error_message=f"Error occured in Python Script name [{file_name}] line number [{line_number}] error message [{str(error)}]".format()    
     return error_message
 
 
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
         
     def __str__(self):
